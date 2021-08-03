a0581d9d36d19376342f9d6a2c8a008fcc980c50
tomasz <tomekpdevelopment@gmail.com>
2021-08-03 20:20:55 +0200

    initial commit with simple models

---
+++ book_outlet/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from django.db.models import Avg
+from .models import Book
+
+# Create your views here.
+def index(request):
+    books = Book.objects.all().order_by("title")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
+
+    return render(request, "book_outlet/index.html", {
+        "books": books,
+        "total_number_of_books": num_books,
+        "average_rating": avg_rating
+    })
+
+def book_detail(request, slug):
+    # try: 
+    #     book = Book.objects.get(pk=id) #pk is primery key
+    # except:
+    #     raise  Http404() 
+    book = get_object_or_404(Book, slug=slug) # right side is what we pass in the function, left side is what we have in Book object (both cases it is slug)
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling,
+    })
