3c31df6273cf041c10fc04af1e5ac76c032fc1f0
tomasz <tomekpdevelopment@gmail.com>
2021-08-03 20:28:49 +0200

    Initial commit with simple models

--- book_store/settings.py
+++ book_store/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&-$j5k0ag_-q+)7v8mhrn$9ppa&mf@u2dmyr252w+4^1@lbag&'
+SECRET_KEY = "${env:SECRET_KEY}"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
